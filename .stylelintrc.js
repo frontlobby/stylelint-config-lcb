15c9ddd1c235249b81893e57056b36ea6f3131e9
Tomas <juuxstar@gmail.com>
2020-03-13 17:05:58 -0400

    disabling rule: no-descending-specificity

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -6,19 +6,20 @@ module.exports = {
 	],
 
 	rules: {
-		"indentation"        : "tab",
-		"linebreaks"         : "unix",
-		"max-empty-lines"    : 2,
-		"no-empty-first-line": true,
-		"no-missing-end-of-source-newline" : true,
+		"indentation"                     : "tab",
+		"linebreaks"                      : "unix",
+		"max-empty-lines"                 : 2,
+		"no-empty-first-line"             : true,
+		"no-missing-end-of-source-newline": true,
+		"no-descending-specificity"       : null,		// disabled because it's too noisy and not enough value
 
 		"at-rule-no-unknown": null,	// disabled because it keeps getting tripped up by LESS variables
 		"declaration-block-no-duplicate-properties" : [ true, {
 			ignore: [ "consecutive-duplicates" ]
 		}],
 
-		"declaration-colon-space-before" : null,		// interferes with juuxstar/declaration-block-align-values
-		"lcbapp/declaration-block-align-values" : true,
+		"declaration-colon-space-before"       : null,		// interferes with juuxstar/declaration-block-align-values
+		"lcbapp/declaration-block-align-values": true,
 
 		// enforce kebab-case for ids and class names
 		"selector-class-pattern" : "^([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
